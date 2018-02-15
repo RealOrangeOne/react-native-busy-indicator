65ebe941e078d347d8caedae98f59b2c910ee8ee
Temitope <temitope@gmail.com>
2018-02-15 04:02:44 -0800

    had to get props directly from new package not React

--- index.js
+++ index.js
@@ -109,17 +109,18 @@ class BusyIndicator extends React.Component {
 }
 
 BusyIndicator.propTypes = {
-  color: React.PropTypes.string,
-  overlayColor: React.PropTypes.string,
-  overlayHeight: React.PropTypes.number,
-  overlayWidth: React.PropTypes.number,
-  size: React.PropTypes.oneOf(['small', 'large']),
-  startVisible: React.PropTypes.bool,
-  text: React.PropTypes.string,
-  textColor: React.PropTypes.string,
-  textFontSize: React.PropTypes.number,
-  textNumberOfLines: React.PropTypes.number
+  color: PropTypes.string,
+  overlayColor: PropTypes.string,
+  overlayHeight: PropTypes.number,
+  overlayWidth: PropTypes.number,
+  size: PropTypes.oneOf(['small', 'large']),
+  startVisible: PropTypes.bool,
+  text: PropTypes.string,
+  textColor: PropTypes.string,
+  textFontSize: PropTypes.number,
+  textNumberOfLines: PropTypes.number
 };
+
 
 BusyIndicator.defaultProps = {
   isDismissible: false,
